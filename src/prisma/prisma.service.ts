55baea4027084385b9c39d08b3949f69f3d1d35b
joão <joaovitormachadororato@gmail.com>
2024-02-12 20:29:00 -0300

    ajust eslint configs and finished prisma module 🔧🔺

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit, INestApplication} from '@nestjs/common';
+import {PrismaClient} from '@prisma/client';
+
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit{
+    
+    async onModuleInit() {
+        this.$connect();
+    }
+
+    async enableShutDownHooks(app : INestApplication){
+        this.$on('beforeExit', async () => {
+            await app.close();
+        })
+    }
+}
